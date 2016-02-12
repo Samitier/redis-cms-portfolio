59a8ddd3b2a066af585762b3aa7fab171b86f1b6
Blai Samitier <blai.samitier@gmail.com>
2016-02-12 20:29:15 +0100

    color changes

--- public/js/directives/app-navbar.js
+++ public/js/directives/app-navbar.js
@@ -2,6 +2,7 @@ module.exports = function() {
     return {
         restrict: 'A',
         link:function(scope, elem) {
+            $(".button-collapse").sideNav();
         },
         templateUrl:'partials/directives/app-header.html'
     };
